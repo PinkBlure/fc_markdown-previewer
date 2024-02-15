924f17ab8d335a7caece7825f2398b36feb2e29e
Aileen <aileenpadrontorres511@gmail.com>
2024-02-15 14:23:56 +0000

    Finished project

--- src/component/Previewer.jsx
+++ src/component/Previewer.jsx
@@ -1,55 +1,45 @@
 import { marked } from "https://esm.sh/marked"
+import React, {useState, useEffect} from "react"
 
 marked.setOptions({
   breaks: true
 })
 
 function Previewer(props) {
+  const [collapsed, setCollapsed] = useState(false)
+  const [htmlContent, setHtmlContent] = useState('')
 
+  useEffect(() => {
+    const generateHtmlContent = () => {
+      setHtmlContent(marked(props.content));
+    };
+
+    generateHtmlContent();
+  }, [props.content]);
+
+  const toggleCollapse = () => {
+    setCollapsed(!collapsed)
+  }
+
+  return (
+    <div className='p-0 border-line'>
+      <div className='card-header d-flex justify-content-between align-items-center rounded-0'>
+        <span>
+          <i className="bi bi-emoji-smile-fill"></i>
+          {' '}Previewer
+        </span>
+        <button type="button" onClick={toggleCollapse}>
+          <i className={collapsed ? "fas fa-window-maximize" : "fas fa-window-minimize"}></i>
+        </button>
+      </div>
+      <div 
+        id='preview' 
+        className={`card-body ${collapsed ? 'collapse' : ''} p-4`} 
+        style={{ height: collapsed ? '0' : 'auto', overflow: 'hidden' }}
+        dangerouslySetInnerHTML={{__html: htmlContent}}>
+      </div>
+    </div>
+  );
 }
-
-// class Previewer extends React.Component {
-//     constructor(props) {
-//       super(props);
-//       this.state = {
-//         collapsed: false,
-//         htmlContent: marked(props.content)
-//       };
-//       this.toggleCollapse = this.toggleCollapse.bind(this);
-//     }
-  
-//     toggleCollapse() {
-//       this.setState({ collapsed: !this.state.collapsed });
-//     }
-    
-//     componentDidUpdate(prevProps) {
-//       if(prevProps.content !== this.props.content) {
-//         const htmlContent = marked(this.props.content)
-//         this.setState({htmlContent})
-//       }
-//     }
-  
-//     render() {
-//       return (
-//         <div className='p-0 border-line'>
-//           <div className='card-header d-flex justify-content-between align-items-center rounded-0'>
-//             <span>
-//               <i className="bi bi-emoji-smile-fill"></i>
-//               {' '}Previewer
-//             </span>
-//             <button type="button" onClick={this.toggleCollapse}>
-//               <i className={this.state.collapsed ? "fas fa-window-maximize" : "fas fa-window-minimize"}></i>
-//             </button>
-//           </div>
-//           <div 
-//             id='preview' 
-//             className={`card-body ${this.state.collapsed ? 'collapse' : ''} p-4`} 
-//             style={{ height: this.state.collapsed ? '0' : 'auto', overflow: 'hidden' }}
-//             dangerouslySetInnerHTML={{__html: this.state.htmlContent}}>
-//           </div>
-//         </div>
-//       );
-//     }
-//   }
 
 export default Previewer
